e6ec72e24b608214be43cd3eeeca119ff30c5fa7
Phillip Lam <plam89@ucla.edu>
2021-04-27 20:19:14 -0700

    added and fixed MU attributes correctly

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,22 +1,28 @@
-import { InputAdornment, TextField, Button, Dialog, DialogTitle, DialogContent } from "@material-ui/core";
+import { InputAdornment, TextField, Button, Dialog, DialogTitle, DialogContent, DialogActions } from "@material-ui/core";
 import {AddBoxOutlined, Link} from '@material-ui/icons'
 import React from "react";
 
 function AddSong() {
   const [dialog, setDialog] = React.useState(false)
 
-  function handleSetDialog() {
+  function handleCloseDialog() {
     setDialog(false)
   }
 
   return (
     <div>
-      <Dialog open={dialog} onClose={handleSetDialog} />
+      <Dialog open={dialog} onClose={handleCloseDialog} />
       <DialogTitle>Edit Song</DialogTitle>
       <DialogContent>
         <img src="https://i1.sndcdn.com/artworks-000670470790-ej1gvb-t500x500.jpg" alt="song thumbnail"></img>
-        <TextField margin="dense" name="title" label="artist" fullWidth/>
+        <TextField margin="dense" name="title" label="title" fullWidth/>
+        <TextField margin="dense" name="artist" label="artist" fullWidth/>
+        <TextField margin="dense" name="thumbnail" label="thumbnail" fullWidth/>
       </DialogContent>
+      <DialogActions>
+        <Button onClick={handleCloseDialog} color="secondary">Cancel</Button>
+        <Button variant="outlined" color="primary">Add Song</Button>
+      </DialogActions>
       <TextField
         placeholder="Paste URL Here"
         fullWidth
