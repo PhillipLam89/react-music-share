76f45ff84c38f01434d98adf2e27d0a1416b00f7
Phillip Lam <plam89@ucla.edu>
2021-06-17 22:23:54 -0700

    testing local queries using apollo client

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -5,7 +5,7 @@
 //  })
 
 //  export default client
-
+import {gql} from 'apollo-boost'
 import ApolloClient from 'apollo-client'
 import { WebSocketLink } from 'apollo-link-ws'
 import { InMemoryCache } from 'apollo-cache-inmemory'
@@ -17,7 +17,38 @@ const client = new ApolloClient({
       reconnect: true
     }
   }),
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(), //typeDefs allows us to write local and custom mutations for Apollo, this will be used to manage state of position of queued songs instead of using React Context or useReducer since those cannot easily access the browser
+  typeDefs: gql `
+    type Song {
+      id: uuid!
+      title: String!
+      artist: String!
+      thumbnail: String!
+      duration: Float!
+      url: String!
+    }
+
+    input SongInput {
+      id: uuid!
+      title: String!
+      artist: String!
+      thumbnail: String!
+      duration: Float!
+      url: String!
+    }
+
+    type Query {
+      queued: [Song]!
+    }
+    type Mutation {
+      addOrRemoveFromQueue(input: SongInput!): [Song]!
+    }
+  `
 })
 
+const data = {
+  queue: []
+}
+
+client.writeData({data})
 export default client
